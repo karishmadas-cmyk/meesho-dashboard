85ed23a0c603b6c76e256bd893f408473ffd3501
karishmadas-cmyk <karishma.das@cjdarcl.com>
2026-09-09 12:26:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -541,11 +541,7 @@ elif page == "DEBIT VIEW":
             name="Weekly Debit Amount"
         )
         
-        fig_w.update_xaxes(
-            dtick="M2",
-            tickformat="%b %Y",
-            title="Month"
-        )
+        update_debit_month_axis(fig_w, sel_months)
         
         fig_w.update_yaxes(
             dtick=2000000,
